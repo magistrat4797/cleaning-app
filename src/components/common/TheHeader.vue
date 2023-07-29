f637a9b99fa47f2b93283f8537c38aba2b36c619
magistrat4797 <vano4797@gmail.com>
2023-07-29 18:00:04 +0200

    Refactored the header's code

--- src/components/common/TheHeader.vue
+++ src/components/common/TheHeader.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-app-bar app :density="isScrolled || isMobile || (isMobile && !isScrolled && sidebar) ? 'default' : 'prominent'" :class="isScrolled || (isMobile && !isScrolled && sidebar) ? 'bg-grey-darken-4' : 'bg-transparent'" class="header" elevation="0">
+  <v-app-bar app :density="density" :class="navClass" class="header" elevation="0">
     <v-container class="d-flex py-0 h-100">
       <v-row align="center" no-gutters>
-        <router-link to="/" class="d-flex align-center h-100" :class="isScrolled || (isMobile && !isScrolled && sidebar) ? 'text-white' : 'text-black'">
+        <router-link to="/" class="d-flex align-center h-100" :class="textColor">
           <logo-icon />
         </router-link>
         <v-spacer />
-        <v-app-bar-nav-icon v-if="isMobile" @click="toggleSidebar()"></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon v-if="isMobile" @click="toggleSidebar"></v-app-bar-nav-icon>
         <v-toolbar-items v-else>
           <v-btn
             v-for="(item, index) in menuItems"
@@ -57,35 +57,15 @@
 </style>
 
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount } from 'vue';
 import useMenu from '@/composables/useMenu';
-import LogoIcon from '@/assets/logo.svg'
+import LogoIcon from '@/assets/logo.svg';
+import { MenuItem } from '@/models/MenuItem';
 
-const sidebar = ref(false);
-const isScrolled = ref(false);
-
-const { menuItems, isMobile } = useMenu(
-  [
-    { title: 'Home', path: '/' },
-    { title: 'About Us', path: '/about' },
-    { title: 'Services', path: '/services' },
-    { title: 'Contact', path: '/contact' },
-  ],
-);
-
-const toggleSidebar = () => {
-  sidebar.value = !sidebar.value;
-}
-
-const handleScroll = () => {
-  isScrolled.value = window.scrollY > 0;
-}
-
-onMounted(() => {
-  window.addEventListener('scroll', handleScroll)
-})
-
-onBeforeUnmount(() => {
-  window.removeEventListener('scroll', handleScroll)  
-});
+const menuItems: MenuItem[] = [
+  { title: 'Home', path: '/' },
+  { title: 'About Us', path: '/about' },
+  { title: 'Services', path: '/services' },
+  { title: 'Contact', path: '/contact' },
+]
+const { isMobile, density, navClass, textColor, sidebar, toggleSidebar } = useMenu(menuItems);
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
+  <div style="min-height: 200vh;">
   Homepage
+  </div>
 </template>
 
 <script lang="ts" setup>
