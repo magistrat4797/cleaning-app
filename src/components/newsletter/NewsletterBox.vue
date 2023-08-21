09a9ac0e5178db94d7460f1ff6944bf4ed57d457
magistrat4797 <vano4797@gmail.com>
2023-08-21 12:08:21 +0200

    Styled the newsletter button

--- src/components/newsletter/NewsletterBox.vue
+++ src/components/newsletter/NewsletterBox.vue
@@ -15,14 +15,18 @@
           </v-col>
           <v-col class="pt-4 pt-md-0 pl-md-6" cols="12" md="6">
             <form>
-              <v-text-field
-                variant="solo"
-                label="Enter Your Email"
-                class="bg-transparent"
-                type="email"
-                hide-details
-              />
-              <base-button type="submit" icon="fa-solid fa-paper-plane" />
+              <div class="d-flex position-relative">
+                <v-text-field
+                  variant="solo"
+                  label="Enter Your Email"
+                  class="bg-transparent newsletter__input"
+                  type="email"
+                  hide-details
+                />
+                <button type="submit" class="newsletter__btn bg-white text-primary rounded">
+                  <font-awesome-icon icon="fa-solid fa-paper-plane" />
+                </button>
+              </div>
             </form>
           </v-col>
         </v-row>
@@ -34,22 +38,50 @@
 <style lang="scss" scoped>
 @import "@/assets/styles/styles.scss";
 
+$box-width: 515px;
+
+$btn-width: 46px;
+$btn-width-md: 56px;
+
 .newsletter {
   margin: 0 auto;
-  max-width: 515px;
+  max-width: $box-width;
   @include md {
     max-width: none;
   }
   &__title {
     line-height: 1.3;
   }
+  &__input {
+    &:deep(.v-field__input) {
+      padding-right: $btn-width;
+    }
+    & + .newsletter__btn {
+      width: $btn-width;
+    }
+    @include md {
+      &:deep(.v-field__input) {
+        padding-right: $btn-width-md;
+      }
+      & + .newsletter__btn {
+        width: $btn-width-md;
+      }
+    }
+  }
+  &__btn {
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    &:hover, &:active {
+      color: $color-secondary!important;
+    }
+  }
 }
 </style>
 
 <script setup lang="ts">
 import { useSlots } from "vue";
-
-import BaseButton from "@/components/base/BaseButton.vue";
 
 const slots = useSlots();
 </script>
